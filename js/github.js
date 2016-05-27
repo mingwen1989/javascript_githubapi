02bf0fcc6e3fa9e7627c135b7d8c7b1049be8b22
themingfu <mwen82@gmail.com>
2016-05-27 10:09:58 -0700

    Add console output

--- js/github.js
+++ js/github.js
@@ -3,8 +3,8 @@ var apiKey = require('./../.env').apiKey;
 exports.Github = function() {
 }
 
-exports.Github.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/themingfu/repos?access_token=' + apiKey).then(function(response){
+exports.Github.prototype.getRepos = function(user){
+  $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
