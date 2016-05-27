93066b629f8eb322bc28219aea68f7457ad6613a
themingfu <mwen82@gmail.com>
2016-05-27 13:18:00 -0700

    Display repo names and descriptions

--- js/github.js
+++ js/github.js
@@ -6,7 +6,7 @@ exports.Github = function(user) {
 
 exports.Github.prototype.getUser = function() {
   $.get('https://api.github.com/users/' + this.user + '?access_token=' + apiKey).then(function(response){
-  			$("userName").html("<h3>Username: " + response.login + "</h3>");
+  			$("#userName").html("<h1>Username: " + response.login + "</h1>");
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
@@ -16,8 +16,8 @@ exports.Github.prototype.getRepos = function(){
   $.get('https://api.github.com/users/' + this.user + '/repos?access_token=' + apiKey).then(function(response){
   		
   		response.forEach( function(repo) {
-  			$("userRepos").append("<h4>" + repo.name + "</h4>");
-  			$("userRepos").append("<h4>" + repo.description + "</h4>");
+  			$("#userRepos").append("<h4>" + repo.name + "</h4>");
+  			$("#userRepos").append("<h4>" + repo.description + "</h4>");
     });
 
   }).fail(function(error){
