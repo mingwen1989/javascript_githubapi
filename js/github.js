4c5565d930046359ffd4247571946794328afb02
themingfu <mwen82@gmail.com>
2016-05-27 13:36:46 -0700

    Display repos and descriptions in a table

--- js/github.js
+++ js/github.js
@@ -6,7 +6,7 @@ exports.Github = function(user) {
 
 exports.Github.prototype.getUser = function() {
   $.get('https://api.github.com/users/' + this.user + '?access_token=' + apiKey).then(function(response){
-  			$("#userName").html("<h1>Username: " + response.login + "</h1>");
+  			$("#userName").html("<h1>" + response.login + "</h1>");
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
@@ -16,8 +16,13 @@ exports.Github.prototype.getRepos = function(){
   $.get('https://api.github.com/users/' + this.user + '/repos?access_token=' + apiKey).then(function(response){
   		
   		response.forEach( function(repo) {
-  			$("#userRepos").append("<h4>" + repo.name + "</h4>");
-  			$("#userRepos").append("<h4>" + repo.description + "</h4>");
+			if(repo.description.length>1){
+  			$("#userRepos").append("<tr><td>"+repo.name+"</td><td>"+repo.description+"</td></tr>");				
+			}
+			else{
+				$("#userRepos").append("<tr><td>"+repo.name+"</td><td></td></tr>");
+			}
+
     });
 
   }).fail(function(error){
